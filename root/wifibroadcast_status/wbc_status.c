43e52d91d3174c21b4f55715c241009d3c1ec941
libc0607 <libc0607@gmail.com>
2019-05-06 22:51:15 +0800

    working on yjsnpi

--- root/wifibroadcast_status/wbc_status.c
+++ root/wifibroadcast_status/wbc_status.c
@@ -20,7 +20,6 @@ int main(int argc, char *argv[]) {
 
     int width, height;
 
-
     int widthpos = width / 2;
     int heightpos = height / offset;
     int fontpos = width / fontscale;
@@ -29,31 +28,33 @@ int main(int argc, char *argv[]) {
 //    fprintf(stderr,"before init\n");
     InitShapes(&width, &height);            // Graphics initialization
 
-
-
     float a = 0;
     for (a=0; a <= 1; a = a + 0.01) {
-	Start(width, height);
-	if (background == 1) { BackgroundRGB(0, 0, 0, 1); };
-	Fill(255, 255, 255, a);
-	TextMid(width / 2, height / offset, text, SansTypeface, width / fontscale);
-	End();
-	usleep(20000);
+		Start(width, height);
+		if (background == 1) { 
+			BackgroundRGB(0, 0, 0, 1); 
+		};
+		Fill(255, 255, 255, a);
+		TextMid(width / 2, height / offset, text, SansTypeface, width / fontscale);
+		End();
+		usleep(20000);
     }
 
     usleep(50000);
 
     for (a=1; a >= 0; a = a - 0.02) {
-	Start(width, height);
-	if (background == 1) { BackgroundRGB(0, 0, 0, 1); };
-	Fill(255, 255, 255, a);
-	TextMid(width / 2, height / offset, text, SansTypeface, width / fontscale);
-	End();
-	usleep(40000);
+		Start(width, height);
+		if (background == 1) { 
+			BackgroundRGB(0, 0, 0, 1); 
+		};
+		Fill(255, 255, 255, a);
+		TextMid(width / 2, height / offset, text, SansTypeface, width / fontscale);
+		End();
+		usleep(40000);
     }
 
     if (background == 1) { // we wait some more to make the splashscreen cover everything (15s)
-	usleep(15000000);
+		usleep(15000000);
     }
 
     End();
