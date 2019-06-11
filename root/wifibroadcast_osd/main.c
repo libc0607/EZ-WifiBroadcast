ae646a3aad5b5bcf91afedce60b3ff88b0ffd699
libc0607 <libc0607@gmail.com>
2019-06-11 12:57:31 +0800

    OSD read telemetry message from UDP, no /root/telemetryfifo1

--- root/wifibroadcast_osd/main.c
+++ root/wifibroadcast_osd/main.c
@@ -3,6 +3,8 @@ Copyright (c) 2015, befinitiv
 Copyright (c) 2012, Broadcom Europe Ltd
 modified by Samuel Brucksch https://github.com/SamuelBrucksch/wifibroadcast_osd
 modified by Rodizio
+modified by libc0607@Github: Get telemetry data from UDP,
+e.g. '/tmp/osd 30000' listen on port 30000
 
 All rights reserved.
 Redistribution and use in source and binary forms, with or without
@@ -39,6 +41,13 @@ SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #include <fcntl.h>
 #include <sys/select.h>
 #include <locale.h>
+#include <endian.h>
+#include <fcntl.h>
+#include <sys/mman.h>
+#include <arpa/inet.h>
+#include <errno.h>
+#include <resolv.h>
+#include <utime.h>
 
 #include "render.h"
 #include "osdconfig.h"
@@ -53,6 +62,9 @@ SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #include "smartport.h"
 #endif
 
+//#define DEBUG
+//#define DUMP_PACKET
+
 long long current_timestamp() {
     struct timeval te;
     gettimeofday(&te, NULL); // get current time
@@ -64,152 +76,205 @@ fd_set set;
 
 struct timeval timeout;
 
-int main(int argc, char *argv[]) {
+void dump_memory(void* p, int length, char * tag)
+{
+	int i, j;
+	unsigned char *addr = (unsigned char *)p;
+
+	printf("\n");
+	printf("===== Memory dump at %s, length=%d =====", tag, length);
+	printf("\n");
+
+	for(i = 0; i < 16; i++)
+		printf("%2x ", i);
+	printf("\n");
+	for(i = 0; i < 16; i++)
+		printf("---");
+	printf("\n");
+	for(i = 0; i < (length/16) + 1; i++) {
+		for(j = 0; j < 16; j++) {
+			if (i * 16 + j >= length)
+				break;
+			printf("%2x ", *(addr + i * 16 + j));
+		}
+		printf("\n");
+	}
+	for(i = 0; i < 16; i++)
+		printf("---");
+	printf("\n\n");
+}
+
+
+int main(int argc, char *argv[]) 
+{
+	uint8_t buf[263]; // Mavlink maximum packet length
+	size_t n;
+	long long fpscount_ts = 0;
+	long long fpscount_ts_last = 0;
+	int fpscount = 0;
+	int fpscount_last = 0;
+	int fps;
+	int do_render = 0;
+	int counter = 0;
+	struct stat fdstatus;
+	telemetry_data_t td;
+	long long prev_time = current_timestamp();
+	long long prev_time2 = current_timestamp();
+	long long prev_cpu_time = current_timestamp();
+	long long delta = 0;
+	int cpuload_gnd = 0;
+	int temp_gnd = 0;
+	int undervolt_gnd = 0;
+	FILE *fp;
+	FILE *fp2;
+	FILE *fp3;
+	long double a[4], b[4];
+	struct sockaddr_in addr;
+	int sockfd;
+	int slen = sizeof(addr);
+	
     fprintf(stderr,"OSD started\n=====================================\n\n");
 
-    setpriority(PRIO_PROCESS, 0, 10);
-
-    setlocale(LC_ALL, "en_GB.UTF-8");
-
-    uint8_t buf[263]; // Mavlink maximum packet length
-    size_t n;
-
-    long long fpscount_ts = 0;
-    long long fpscount_ts_last = 0;
-    int fpscount = 0;
-    int fpscount_last = 0;
-    int fps;
-
-    int do_render = 0;
-    int counter = 0;
-
-
+	setpriority(PRIO_PROCESS, 0, 10);
+	setlocale(LC_ALL, "en_GB.UTF-8");
+	signal(SIGPIPE, SIG_IGN);
+	
 #ifdef FRSKY
     frsky_state_t fs;
 #endif
-
-    struct stat fdstatus;
-    signal(SIGPIPE, SIG_IGN);
-    char fifonam[100];
-    sprintf(fifonam, "/root/telemetryfifo1");
-
-    int readfd;
-    readfd = open(fifonam, O_RDONLY | O_NONBLOCK);
-    if(-1==readfd) {
-        perror("ERROR: Could not open /root/telemetryfifo1");
-        exit(EXIT_FAILURE);
-    }
-    if(-1==fstat(readfd, &fdstatus)) {
-        perror("ERROR: fstat /root/telemetryfifo1");
-        close(readfd);
-        exit(EXIT_FAILURE);
-    }
-
+ 
+#ifdef DEBUG
     fprintf(stderr,"OSD: Initializing sharedmem ...\n");
-    telemetry_data_t td;
+#endif
     telemetry_init(&td);
+#ifdef DEBUG
     fprintf(stderr,"OSD: Sharedmem init done\n");
+#endif
 
-//    fprintf(stderr,"OSD: Initializing render engine ...\n");
+#ifdef DEBUG
+	fprintf(stderr,"OSD: Initializing render engine ...\n");
+#endif
     render_init();
-//    fprintf(stderr,"OSD: Render init done\n");
+#ifdef DEBUG
+	fprintf(stderr,"OSD: Render init done\n");
+#endif
 
-    long long prev_time = current_timestamp();
-    long long prev_time2 = current_timestamp();
-
-    long long prev_cpu_time = current_timestamp();
-    long long delta = 0;
-
-    int cpuload_gnd = 0;
-    int temp_gnd = 0;
-    int undervolt_gnd = 0;
-    FILE *fp;
-    FILE *fp2;
-    FILE *fp3;
-    long double a[4], b[4];
-
+	// UDP Socket
+	bzero(&addr, sizeof(addr));
+	addr.sin_family = AF_INET;
+	addr.sin_port = htons(atoi(argv[1]));
+	fprintf(stderr, "UDP Listen port %s", argv[1]);
+	addr.sin_addr.s_addr = htonl(INADDR_ANY);
+	if ( (sockfd = socket(PF_INET, SOCK_DGRAM, 0)) < 0 ){
+		fprintf(stderr, "ERROR: Could not create UDP socket!");
+		exit(1);
+	}
+	bind(sockfd, (struct sockaddr*)&addr, sizeof(addr));
+	
+	// Undervolt
     fp3 = fopen("/tmp/undervolt","r");
     if(NULL == fp3) {
         perror("ERROR: Could not open /tmp/undervolt");
         exit(EXIT_FAILURE);
     }
-    fscanf(fp3,"%d",&undervolt_gnd);
+    fscanf(fp3, "%d", &undervolt_gnd);
     fclose(fp3);
-//    fprintf(stderr,"undervolt:%d\n",undervolt_gnd);
+#ifdef DEBUG
+	fprintf(stderr,"undervolt:%d\n",undervolt_gnd);
+#endif
 
+	// main loop
     while(1) {
+#ifdef DEBUG
 //		fprintf(stderr," start while ");
-//		prev_time = current_timestamp();
-
+		prev_time = current_timestamp();
+#endif
 	    FD_ZERO(&set);
-	    FD_SET(readfd, &set);
+	    FD_SET(sockfd, &set);
 	    timeout.tv_sec = 0;
 	    timeout.tv_usec = 50 * 1000;
 	    // look for data 50ms, then timeout
-	    n = select(readfd + 1, &set, NULL, NULL, &timeout);
-	    if(n > 0) { // if data there, read it and parse it
-	        n = read(readfd, buf, sizeof(buf));
-//	        printf("OSD: %d bytes read\n",n);
-	        if(n == 0) { continue; } // EOF
-		if(n<0) {
-		    perror("OSD: read");
-		    exit(-1);
-		}
+	    n = select(sockfd + 1, &set, NULL, NULL, &timeout);
+	    if(n > 0) { 
+			// if data there, read it and parse it
+			n = recvfrom(sockfd, (char *)&buf, sizeof(buf), 0, 
+					(struct sockaddr*)&addr, &slen);
+#ifdef DEBUG
+			fprintf(stderr, "Get data from %s, %d Bytes\n", inet_ntoa(addr.sin_addr), n);
+#ifdef DUMP_PACKET
+			dump_memory(&buf, n, "buf");
+#endif
+#endif
+	        if(n == 0) 
+				continue;			// EOF
+			if (n < 0) {
+				perror("OSD: recvfrom");
+				exit(-1);
+			}
 #ifdef FRSKY
-		frsky_parse_buffer(&fs, &td, buf, n);
+			frsky_parse_buffer(&fs, &td, buf, n);
 #elif defined(LTM)
-		do_render = ltm_read(&td, buf, n);
+			do_render = ltm_read(&td, buf, n);
 #elif defined(MAVLINK)
-		do_render = mavlink_read(&td, buf, n);
+			do_render = mavlink_read(&td, buf, n);
 #elif defined(SMARTPORT)
-		smartport_read(&td, buf, n);
+			smartport_read(&td, buf, n);
 #endif
 	    }
 	    counter++;
+#ifdef DEBUG
 //	    fprintf(stderr,"OSD: counter: %d\n",counter);
+#endif
 	    // render only if we have data that needs to be processed as quick as possible (attitude)
 	    // or if three iterations (~150ms) passed without rendering
 	    if ((do_render == 1) || (counter == 3)) {
-//		fprintf(stderr," rendering! ");
-		prev_time = current_timestamp();
-		fpscount++;
-		render(&td, cpuload_gnd, temp_gnd/1000, undervolt_gnd,fps);
-		long long took = current_timestamp() - prev_time;
-//		fprintf(stderr,"Render took %lldms\n", took);
-		do_render = 0;
-		counter = 0;
+#ifdef DEBUG			
+//			fprintf(stderr," rendering! ");
+#endif
+			prev_time = current_timestamp();
+			fpscount++;
+			render(&td, cpuload_gnd, temp_gnd/1000, undervolt_gnd,fps);
+			long long took = current_timestamp() - prev_time;
+#ifdef DEBUG
+//			fprintf(stderr,"Render took %lldms\n", took);
+#endif
+			do_render = 0;
+			counter = 0;
 	    }
 
 	    delta = current_timestamp() - prev_cpu_time;
 	    if (delta > 1000) {
-		prev_cpu_time = current_timestamp();
-//		fprintf(stderr,"delta > 10000\n");
+			prev_cpu_time = current_timestamp();
+	//		fprintf(stderr,"delta > 10000\n");
 
-		fp2 = fopen("/sys/class/thermal/thermal_zone0/temp","r");
-		fscanf(fp2,"%d",&temp_gnd);
-		fclose(fp2);
-//		fprintf(stderr,"temp gnd:%d\n",temp_gnd/1000);
+			fp2 = fopen("/sys/class/thermal/thermal_zone0/temp","r");
+			fscanf(fp2,"%d",&temp_gnd);
+			fclose(fp2);
+	//		fprintf(stderr,"temp gnd:%d\n",temp_gnd/1000);
 
-		fp = fopen("/proc/stat","r");
-		fscanf(fp,"%*s %Lf %Lf %Lf %Lf",&a[0],&a[1],&a[2],&a[3]);
-		fclose(fp);
+			fp = fopen("/proc/stat","r");
+			fscanf(fp,"%*s %Lf %Lf %Lf %Lf",&a[0],&a[1],&a[2],&a[3]);
+			fclose(fp);
 
-		cpuload_gnd = (((b[0]+b[1]+b[2]) - (a[0]+a[1]+a[2])) / ((b[0]+b[1]+b[2]+b[3]) - (a[0]+a[1]+a[2]+a[3]))) * 100;
-//		fprintf(stderr,"cpuload gnd:%d\n",cpuload_gnd);
+			cpuload_gnd = (((b[0]+b[1]+b[2]) - (a[0]+a[1]+a[2])) / ((b[0]+b[1]+b[2]+b[3]) - (a[0]+a[1]+a[2]+a[3]))) * 100;
+	//		fprintf(stderr,"cpuload gnd:%d\n",cpuload_gnd);
 
-		fp = fopen("/proc/stat","r");
-		fscanf(fp,"%*s %Lf %Lf %Lf %Lf",&b[0],&b[1],&b[2],&b[3]);
-		fclose(fp);
+			fp = fopen("/proc/stat","r");
+			fscanf(fp,"%*s %Lf %Lf %Lf %Lf",&b[0],&b[1],&b[2],&b[3]);
+			fclose(fp);
 	    }
-//		long long took = current_timestamp() - prev_time;
+#ifdef DEBUG
+		long long took = current_timestamp() - prev_time;
 //		fprintf(stderr,"while took %lldms\n", took);
-
+#endif
 		long long fpscount_timer = current_timestamp() - fpscount_ts_last;
 		if (fpscount_timer > 2000) {
 		    fpscount_ts_last = current_timestamp();
 		    fps = (fpscount - fpscount_last) / 2;
 		    fpscount_last = fpscount;
-//		    fprintf(stderr,"OSD FPS: %d\n", fps);
+#ifdef DEBUG
+		    fprintf(stderr,"OSD FPS: %d\n", fps);
+#endif
 		}
     }
     return 0;
