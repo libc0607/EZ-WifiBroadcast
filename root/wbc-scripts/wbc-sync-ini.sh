b0468c13e4e5e43eeb47f8e56693d2b615dc3587
libc0607 <libc0607@gmail.com>
2019-06-27 23:31:21 +0800

    (Set & limit monit logfile) & Bug fixes

--- root/wbc-scripts/wbc-sync-ini.sh
+++ root/wbc-scripts/wbc-sync-ini.sh
@@ -3,7 +3,7 @@ source /root/wbc-scripts/env.sh
 source /root/wbc-scripts/func_logconf.sh
 
 while true; do
-	wget http://$OPENWRT_IP/$OPENWRT_CONF_PATH -O /var/run/wbc/config.ini >/dev/null 2>&1
+	wget -t 1 -T 1 http://$OPENWRT_IP/$OPENWRT_CONF_PATH -O /var/run/wbc/config.ini >/dev/null 2>&1
 	if [ ! -f "$OPENWRT_CONFIG_FILE" ]; then
 		/bin/bash /root/wbc-scripts/wbc-air-genconf.sh
 	fi
