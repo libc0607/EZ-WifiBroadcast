09be24dfd7c15aaec994b450ab4cf2ae3adf4c18
libc0607 <libc0607@gmail.com>
2019-09-24 23:39:58 +0800

    tx_rc_sbus: wip

--- root/wifibroadcast/tx_rc_sbus.c
+++ root/wifibroadcast/tx_rc_sbus.c
@@ -39,19 +39,21 @@ password=1145141919810
 #include <strings.h>
 #include <stropts.h>
 #include <sys/mman.h>
+#include <sys/random.h>
 #include <sys/resource.h>
 #include <sys/stat.h>
 #include <sys/types.h>
 #include <time.h>
 #include <unistd.h>
 
-// Note: redefinition issue -- use setsbus
+// Note: redefinition issue -- use setsbus first
 //#include <asm/termios.h>
 #include <sys/ioctl.h>
 
 int sockfd;
 int uartfd;
 int udpfd;
+int urandom_fd;
 
 static uint8_t u8aRadiotapHeader[] = {
 	0x00, 0x00, // <-- radiotap version
@@ -77,18 +79,70 @@ static uint8_t u8aIeeeHeader_rts[] = {
         0xff, // 1st byte of IEEE802.11 RA (mac) must be 0xff or something odd (wifi hardware determines broadcast/multicast through odd/even check)
 };
 
-struct framedata_s {
+#define SBUS_FRAME_LENGTH_BYTE 25
+#define FRAME_DATA_OFFSET 32
+struct framedata_s 
+{
 	// 16 bytes info
-	uint32_t crc;					// wip, 0x00000000 now
+	uint32_t crc;					// wip
 	uint32_t length;				// body length
     uint32_t seqnumber;
-	uint8_t info[4];				// for future use, now fill with 0
+	uint32_t info;					// information of this packet, 0x0 now
+	
+	// 16 bytes for future use
+	uint32_t random;				// random number; prevent replay attack (with encrypt enabled)
+	uint32_t zero[3];				// for future use
 	
 	// payload
-    uint8_t data[25];
+    uint8_t data[SBUS_FRAME_LENGTH_BYTE];
 
 } __attribute__ ((__packed__));
-#define FRAME_DATA_OFFSET 16
+
+static const uint32_t crc32_table[] =
+{
+  0x00000000, 0x04c11db7, 0x09823b6e, 0x0d4326d9, 0x130476dc, 0x17c56b6b, 0x1a864db2, 0x1e475005,
+  0x2608edb8, 0x22c9f00f, 0x2f8ad6d6, 0x2b4bcb61, 0x350c9b64, 0x31cd86d3, 0x3c8ea00a, 0x384fbdbd, 
+  0x4c11db70, 0x48d0c6c7, 0x4593e01e, 0x4152fda9, 0x5f15adac, 0x5bd4b01b, 0x569796c2, 0x52568b75,
+  0x6a1936c8, 0x6ed82b7f, 0x639b0da6, 0x675a1011, 0x791d4014, 0x7ddc5da3, 0x709f7b7a, 0x745e66cd,
+  0x9823b6e0, 0x9ce2ab57, 0x91a18d8e, 0x95609039, 0x8b27c03c, 0x8fe6dd8b, 0x82a5fb52, 0x8664e6e5,
+  0xbe2b5b58, 0xbaea46ef, 0xb7a96036, 0xb3687d81, 0xad2f2d84, 0xa9ee3033, 0xa4ad16ea, 0xa06c0b5d,
+  0xd4326d90, 0xd0f37027, 0xddb056fe, 0xd9714b49, 0xc7361b4c, 0xc3f706fb, 0xceb42022, 0xca753d95,
+  0xf23a8028, 0xf6fb9d9f, 0xfbb8bb46, 0xff79a6f1, 0xe13ef6f4, 0xe5ffeb43, 0xe8bccd9a, 0xec7dd02d,
+  0x34867077, 0x30476dc0, 0x3d044b19, 0x39c556ae, 0x278206ab, 0x23431b1c, 0x2e003dc5, 0x2ac12072,
+  0x128e9dcf, 0x164f8078, 0x1b0ca6a1, 0x1fcdbb16, 0x018aeb13, 0x054bf6a4, 0x0808d07d, 0x0cc9cdca,
+  0x7897ab07, 0x7c56b6b0, 0x71159069, 0x75d48dde, 0x6b93dddb, 0x6f52c06c, 0x6211e6b5, 0x66d0fb02,
+  0x5e9f46bf, 0x5a5e5b08, 0x571d7dd1, 0x53dc6066, 0x4d9b3063, 0x495a2dd4, 0x44190b0d, 0x40d816ba,
+  0xaca5c697, 0xa864db20, 0xa527fdf9, 0xa1e6e04e, 0xbfa1b04b, 0xbb60adfc, 0xb6238b25, 0xb2e29692,
+  0x8aad2b2f, 0x8e6c3698, 0x832f1041, 0x87ee0df6, 0x99a95df3, 0x9d684044, 0x902b669d, 0x94ea7b2a,
+  0xe0b41de7, 0xe4750050, 0xe9362689, 0xedf73b3e, 0xf3b06b3b, 0xf771768c, 0xfa325055, 0xfef34de2,
+  0xc6bcf05f, 0xc27dede8, 0xcf3ecb31, 0xcbffd686, 0xd5b88683, 0xd1799b34, 0xdc3abded, 0xd8fba05a,
+  0x690ce0ee, 0x6dcdfd59, 0x608edb80, 0x644fc637, 0x7a089632, 0x7ec98b85, 0x738aad5c, 0x774bb0eb,
+  0x4f040d56, 0x4bc510e1, 0x46863638, 0x42472b8f, 0x5c007b8a, 0x58c1663d, 0x558240e4, 0x51435d53,
+  0x251d3b9e, 0x21dc2629, 0x2c9f00f0, 0x285e1d47, 0x36194d42, 0x32d850f5, 0x3f9b762c, 0x3b5a6b9b,
+  0x0315d626, 0x07d4cb91, 0x0a97ed48, 0x0e56f0ff, 0x1011a0fa, 0x14d0bd4d, 0x19939b94, 0x1d528623,
+  0xf12f560e, 0xf5ee4bb9, 0xf8ad6d60, 0xfc6c70d7, 0xe22b20d2, 0xe6ea3d65, 0xeba91bbc, 0xef68060b,
+  0xd727bbb6, 0xd3e6a601, 0xdea580d8, 0xda649d6f, 0xc423cd6a, 0xc0e2d0dd, 0xcda1f604, 0xc960ebb3,
+  0xbd3e8d7e, 0xb9ff90c9, 0xb4bcb610, 0xb07daba7, 0xae3afba2, 0xaafbe615, 0xa7b8c0cc, 0xa379dd7b,
+  0x9b3660c6, 0x9ff77d71, 0x92b45ba8, 0x9675461f, 0x8832161a, 0x8cf30bad, 0x81b02d74, 0x857130c3,
+  0x5d8a9099, 0x594b8d2e, 0x5408abf7, 0x50c9b640, 0x4e8ee645, 0x4a4ffbf2, 0x470cdd2b, 0x43cdc09c,
+  0x7b827d21, 0x7f436096, 0x7200464f, 0x76c15bf8, 0x68860bfd, 0x6c47164a, 0x61043093, 0x65c52d24,
+  0x119b4be9, 0x155a565e, 0x18197087, 0x1cd86d30, 0x029f3d35, 0x065e2082, 0x0b1d065b, 0x0fdc1bec,
+  0x3793a651, 0x3352bbe6, 0x3e119d3f, 0x3ad08088, 0x2497d08d, 0x2056cd3a, 0x2d15ebe3, 0x29d4f654,
+  0xc5a92679, 0xc1683bce, 0xcc2b1d17, 0xc8ea00a0, 0xd6ad50a5, 0xd26c4d12, 0xdf2f6bcb, 0xdbee767c,
+  0xe3a1cbc1, 0xe760d676, 0xea23f0af, 0xeee2ed18, 0xf0a5bd1d, 0xf464a0aa, 0xf9278673, 0xfde69bc4,
+  0x89b8fd09, 0x8d79e0be, 0x803ac667, 0x84fbdbd0, 0x9abc8bd5, 0x9e7d9662, 0x933eb0bb, 0x97ffad0c,
+  0xafb010b1, 0xab710d06, 0xa6322bdf, 0xa2f33668, 0xbcb4666d, 0xb8757bda, 0xb5365d03, 0xb1f740b4
+};
+
+uint32_t xcrc32 (const unsigned char *buf, int len, unsigned int init)
+{
+	unsigned int crc = init;
+	while (len--) {
+		crc = (crc << 8) ^ crc32_table[((crc >> 24) ^ *buf) & 255];
+		buf++;
+	}
+	return crc;
+}
 
 void usage(void)
 {
@@ -166,33 +220,6 @@ wifibroadcast_rx_status_t *telemetry_wbc_status_memory_open (void)
 void telemetry_init(telemetry_data_t *td) 
 {
     td->rx_status = telemetry_wbc_status_memory_open();
-}
-
-void set_baud_custom (int fd, int speed)
-{
-	struct serial_struct ss;
-	if (ioctl(fd, TIOCGSERIAL, &ss) < 0) {
-		perror("TIOCGSERIAL failed");
-		exit(1);
-	}
-
-	ss.flags = (ss.flags & ~ASYNC_SPD_MASK) | ASYNC_SPD_CUST;
-	ss.custom_divisor = (ss.baud_base + (speed/2)) / speed;
-	int closest_speed = ss.baud_base / ss.custom_divisor;
-
-	if (closest_speed < speed * 98 / 100 || closest_speed > speed * 102 / 100) {
-		fprintf(stderr, "Cannot set speed to %d, closest is %d\n", speed, closest_speed);
-		exit(1);
-	}
-
-	fprintf(stderr, "closest baud = %i, base = %i, divisor = %i\n", closest_speed, ss.baud_base,
-			ss.custom_divisor);
-
-	if (ioctl(fd, TIOCSSERIAL, &ss) < 0) {
-		perror("TIOCSSERIAL failed");
-		exit(1);
-	}
-	return;
 }
 
 uint8_t bitrate_to_rtap8 (int bitrate) 
@@ -312,11 +339,23 @@ int packet_ieeeheader_init (int offset, uint8_t * buf, dictionary *ini)
 	return sizeof(u8aIeeeHeader_rts);
 }
 
+int encrypt_payload(uint8_t * buf, size_t length, int encrypt_en, char * pwd) 
+{
+	if (0 == encrypt_en) {
+		return length;
+	}
+	size_t enc_len;
+	uint8_t * enc_data = xxtea_encrypt(buf, length, pwd, &enc_len);
+	// overwrite buffer
+	memcpy(buf, enc_data, enc_len);
+	return enc_len;
+}
+
 int main (int argc, char *argv[]) 
 {
 	struct framedata_s framedata;
 	uint8_t framedata_body_length = sizeof(framedata);
-	uint8_t buf[512];
+	uint8_t buf[128];
 	bzero(buf, sizeof(buf));
 	
 	// Get ini from file
@@ -394,15 +433,19 @@ int main (int argc, char *argv[])
 	uint32_t seqno = 0;
 	int k = 0;
 	int uart_read_len = 0;
-	int full_packet_size = rtheader_length + ieeeheader_length + sizeof(framedata);
 	int full_header_length = rtheader_length + ieeeheader_length;
+	int encrypted_data_length = 0;
+	size_t rand_size = -1;
+	uint8_t uart_read_buf[64] = {0};
 	
 	while (1) {
 		// 1. Get data from UART
-		uart_read_len = read(uartfd, buf +full_header_length +FRAME_DATA_OFFSET, 25);
+		//uart_read_len = read(uartfd, buf+full_header_length+FRAME_DATA_OFFSET, SBUS_FRAME_LENGTH_BYTE);
+		bzero(uart_read_buf, sizeof(uart_read_buf));
+		uart_read_len = read(uartfd, uart_read_buf, SBUS_FRAME_LENGTH_BYTE);
 		if (param_debug) {
 			fprintf(stderr, "uart read() got %d bytes\n", uart_read_len);
-			dump_memory(buf +full_header_length +FRAME_DATA_OFFSET, uart_read_len, "uart read");
+			dump_memory(buf+full_header_length+FRAME_DATA_OFFSET, uart_read_len, "uart read");
 		}
 		if (uart_read_len == 0)
 			continue;
@@ -411,36 +454,45 @@ int main (int argc, char *argv[])
 			continue;
 		}
 		
-		// 2. fill frame seqno
-		framedata.seqnumber = htonl(seqno);
-		
-		// 3. calculate crc
-		// pass
-		
-		// 4.1 Send frame to air
-		if (param_mode == 0 || param_mode == 2) {
-			for (k = 0; k < param_retrans; k++) {
-				if ( write(sockfd, buf, full_packet_size) < 0 ) {
-					fprintf(stderr, "injection failed, seqno=%d\n", seqno);	// send failed
-				}
-				usleep(2000);
+		// 2. send packet
+		for (k = 0; k < param_retrans; k++) {
+			
+			// 2.0 clean buffer
+			bzero(buf+full_header_length+FRAME_DATA_OFFSET, sizeof(framedata)-FRAME_DATA_OFFSET);
+			
+			// 2.1 fill frame (seqno, data, info(wip...) )
+			framedata.seqnumber = htonl(seqno);
+			memcpy(buf+full_header_length+FRAME_DATA_OFFSET, uart_read_buf, uart_read_len);
+			
+			// 2.2 get random number
+			while (rand_size == -1) {
+				rand_size = getrandom(&(framedata.random), sizeof(framedata.random), 0);
 			}
-		}
 		
-		// 4.2 Send frame to UDP
-		if (param_mode == 1 || param_mode == 2) {
-			for (k = 0; k < param_retrans; k++) {
-				if (sendto(udpfd, buf +full_header_length, framedata_body_length, 0, 
+			// 2.3 calculate crc32: fill 0xffffffff first
+			framedata.crc = 0xffffffff;
+			framedata.crc = xcrc32(buf+full_header_length, sizeof(framedata), 0xffffffff);
+			
+			// 2.4 encrypt full packet
+			encrypted_data_length = encrypt_payload(buf+full_header_length, 
+									sizeof(framedata), param_encrypt, param_password);
+			
+			// 2.5 send packet 
+			if (param_mode == 0 || param_mode == 2) {	
+				// 2.5.1 wi-fi
+				if ( write(sockfd, buf, full_header_length + encrypted_data_length) < 0 ) {
+					fprintf(stderr, "ERROR: wireless injection failed, seqno=%d\n", seqno);	
+				}
+			} else if (param_mode == 1 || param_mode == 2) {
+				// 2.5.2 udp
+				if (sendto(udpfd, buf +full_header_length, encrypted_data_length, 0, 
 									(struct sockaddr*)&send_addr, slen) == -1) {
 					fprintf(stderr, "ERROR: UDP Could not send data! seqno=%d\n", seqno);
 				} 
-				usleep(2000); 
 			}
+			usleep(2000);
 		}
-		
-		// 
 		seqno++;
-//		usleep(3000);	// wait a bit (if in nonblock mode)
 	}
 	
 	close(sockfd);
@@ -448,6 +500,8 @@ int main (int argc, char *argv[])
 		close(uartfd);
 	if (param_mode == 1 || param_mode == 2)
 		close(udpfd);
+	close(urandom_fd);
 	iniparser_freedict(ini);
 	return EXIT_SUCCESS;
 }
+
